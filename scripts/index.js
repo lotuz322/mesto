751c5ab8a3518e91a3624e0e5804cae6441a7779
Daniil Fedorov <GAIND@yandex.ru>
2021-12-05 22:47:26 +0300

    feat: добавить добавление карт

--- scripts/index.js
+++ scripts/index.js
@@ -1,37 +1,75 @@
-const formElement = document.querySelector(".popup__container");
-formElement.addEventListener('submit', (evt) => {
-  togglePopUp();
-  formSubmitHandler(evt);
-});
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+const galleryItemTemplate = document.querySelector('#gallery__item').content.querySelector('.photo-gallery__item');
+const gallery = document = document.querySelector('.photo-gallery');
+initialCards.forEach(item => addCard(item.name, item.link));
 
-const nameInput = formElement.querySelector("#name");
-const aboutMeInput = formElement.querySelector("#about-me");
-
-const popUp = document.querySelector(".popup");
-
+const popUpEditProfile = document.querySelector(".popup-edit-profile");
+const nameInputPopUpEdit = popUpEditProfile.querySelector("#name");
+const aboutMeInputPopUpEdit = popUpEditProfile.querySelector("#about-me");
 const profileName = document.querySelector(".profile__name");
 const profileAboutMe = document.querySelector(".profile__about-me");
-
-const editButton = document.querySelector(".profile__edit-btn");
-editButton.addEventListener('click', () => {
-  popUpUpdate();
-  togglePopUp();
+popUpEditProfile.querySelector(".popup__container").addEventListener('submit', evt => {
+  evt.preventDefault();
+  togglePopUp(popUpEditProfile);
+  profileName.textContent = nameInputPopUpEdit.value;
+  profileAboutMe.textContent = aboutMeInputPopUpEdit.value;
 });
 
-const closePopUp = document.querySelector(".popup__close-btn");
-closePopUp.addEventListener("click", togglePopUp);
-
-function togglePopUp() {
-  popUp.classList.toggle("popup_opened");
-}
-
-function popUpUpdate() {
-  nameInput.value = profileName.textContent;
-  aboutMeInput.value = profileAboutMe.textContent;
-}
-
-function formSubmitHandler (evt) {
+const popUpAddCard = document.querySelector(".popup-add-card");
+const nameInputPopUpAddCard = popUpAddCard.querySelector("#add-card-name");
+const urlInputPopUpAddCard = popUpAddCard.querySelector("#add-card-url");
+popUpAddCard.querySelector(".popup__container").addEventListener('submit', (evt) => {
   evt.preventDefault();
-  profileName.textContent = nameInput.value;
-  profileAboutMe.textContent = aboutMeInput.value;
+  togglePopUp(popUpAddCard);
+  addCard(nameInputPopUpAddCard.value, urlInputPopUpAddCard.value);
+});
+
+document.querySelector(".profile__edit-btn").addEventListener('click', () => {
+  nameInputPopUpEdit.value = profileName.textContent;
+  aboutMeInputPopUpEdit.value = profileAboutMe.textContent;
+  togglePopUp(popUpEditProfile);
+});
+
+document.querySelector(".profile__add-btn").addEventListener('click', () => {
+  togglePopUp(popUpAddCard);
+});
+
+document.querySelectorAll(".popup__close-btn")
+  .forEach(item => item.addEventListener('click', evt => {
+    togglePopUp(evt.target.closest('.popup'))}));
+
+function togglePopUp(targetPopUp) {
+  targetPopUp.classList.toggle("popup_opened");
+}
+
+function addCard(name, link) {
+  const galleryItemElement = galleryItemTemplate.cloneNode(true);
+  galleryItemElement.querySelector('.photo-gallery__image').src = link;
+  galleryItemElement.querySelector('.photo-gallery__name').textContent = name;
+  gallery.prepend(galleryItemElement);
 }
