7d3e8294ab84f8ef6b39d83e6aa788b3025d0094
Daniil Fedorov <GAIND@yandex.ru>
2021-12-27 16:22:05 +0300

    feat: просмотр фото

--- scripts/index.js
+++ scripts/index.js
@@ -28,26 +28,30 @@ const galleryItemTemplate = document.querySelector('#gallery__item').content.que
 const gallery = document = document.querySelector('.photo-gallery');
 initialCards.forEach(item => addCard(item.name, item.link));
 
-const popUpEditProfile = document.querySelector(".popup-edit-profile");
-const nameInputPopUpEdit = popUpEditProfile.querySelector("#name");
-const aboutMeInputPopUpEdit = popUpEditProfile.querySelector("#about-me");
-const profileName = document.querySelector(".profile__name");
-const profileAboutMe = document.querySelector(".profile__about-me");
-popUpEditProfile.querySelector(".popup__container").addEventListener('submit', evt => {
+const popUpEditProfile = document.querySelector('.popup-edit-profile');
+const nameInputPopUpEdit = popUpEditProfile.querySelector('#name');
+const aboutMeInputPopUpEdit = popUpEditProfile.querySelector('#about-me');
+const profileName = document.querySelector('.profile__name');
+const profileAboutMe = document.querySelector('.profile__about-me');
+popUpEditProfile.querySelector('.popup__container').addEventListener('submit', evt => {
   evt.preventDefault();
   togglePopUp(popUpEditProfile);
   profileName.textContent = nameInputPopUpEdit.value;
   profileAboutMe.textContent = aboutMeInputPopUpEdit.value;
 });
 
-const popUpAddCard = document.querySelector(".popup-add-card");
-const nameInputPopUpAddCard = popUpAddCard.querySelector("#add-card-name");
-const urlInputPopUpAddCard = popUpAddCard.querySelector("#add-card-url");
-popUpAddCard.querySelector(".popup__container").addEventListener('submit', (evt) => {
+const popUpAddCard = document.querySelector('.popup-add-card');
+const nameInputPopUpAddCard = popUpAddCard.querySelector('#add-card-name');
+const urlInputPopUpAddCard = popUpAddCard.querySelector('#add-card-url');
+popUpAddCard.querySelector('.popup__container').addEventListener('submit', (evt) => {
   evt.preventDefault();
   togglePopUp(popUpAddCard);
   addCard(nameInputPopUpAddCard.value, urlInputPopUpAddCard.value);
 });
+
+const popUpViewImage = document.querySelector('.popup-view-image');
+const srcPopUpViewImage = popUpViewImage.querySelector('.popup__image');
+const paragraphPopUpViewImage = popUpViewImage.querySelector('.popup__paragraph');
 
 document.querySelector(".profile__edit-btn").addEventListener('click', () => {
   nameInputPopUpEdit.value = profileName.textContent;
@@ -71,5 +75,16 @@ function addCard(name, link) {
   const galleryItemElement = galleryItemTemplate.cloneNode(true);
   galleryItemElement.querySelector('.photo-gallery__image').src = link;
   galleryItemElement.querySelector('.photo-gallery__name').textContent = name;
+  galleryItemElement.querySelector('.photo-gallery__like-btn').addEventListener('click', evt => {
+    evt.target.classList.toggle('photo-gallery__like-btn_active')
+  });
+  galleryItemElement.querySelector('.photo-gallery__delete-btn').addEventListener('click', evt => {
+    evt.target.closest('.photo-gallery__item').remove();
+  });
+  galleryItemElement.querySelector('.photo-gallery__image').addEventListener('click', () => {
+    togglePopUp(popUpViewImage);
+    srcPopUpViewImage.src = link;
+    paragraphPopUpViewImage.textContent = name;
+  });
   gallery.prepend(galleryItemElement);
 }
