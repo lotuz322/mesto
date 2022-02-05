c9204bc8d123d718b223c1509cbf155147bd1dbb
Daniil Fedorov <GAIND@yandex.ru>
2022-02-05 05:39:32 +0300

    feat: добавить атрибут alt модальному окну

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,38 @@
 const initialCards = [
   {
     name: 'Архыз',
+    alt: 'Горы Архыза',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
   },
   {
     name: 'Челябинская область',
+    alt: 'Река в Челябинской области',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
   },
   {
     name: 'Иваново',
+    alt: 'Бетонные джунгли Иваново',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
   },
   {
     name: 'Камчатка',
+    alt: 'Горы Камчатки',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
   },
   {
     name: 'Холмогорский район',
+    alt: 'Леса холмогорского района',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
   },
   {
     name: 'Байкал',
+    alt: 'Озеро Байкал',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
 ];
 const galleryItemTemplate = document.querySelector('#gallery__item').content.querySelector('.photo-gallery__item');
 const gallery = document = document.querySelector('.photo-gallery');
-initialCards.forEach(item => renderCard(item.name, item.link));
+initialCards.forEach(item => renderCard(item.name, item.link, item.alt));
 
 const popUpEditProfile = document.querySelector('.popup-edit-profile');
 const nameInputPopUpEdit = popUpEditProfile.querySelector('#name');
@@ -46,7 +52,7 @@ const urlInputPopUpAddCard = popUpAddCard.querySelector('#add-card-url');
 popUpAddCard.querySelector('.popup__container').addEventListener('submit', (evt) => {
   evt.preventDefault();
   togglePopUp(popUpAddCard);
-  renderCard(nameInputPopUpAddCard.value, urlInputPopUpAddCard.value);
+  renderCard(nameInputPopUpAddCard.value, urlInputPopUpAddCard.value, nameInputPopUpAddCard.value);
 });
 
 const popUpViewImage = document.querySelector('.popup-view-image');
@@ -71,7 +77,7 @@ function togglePopUp(targetPopUp) {
   targetPopUp.classList.toggle("popup_opened");
 }
 
-function addCard(name, link) {
+function addCard(name, link, alt) {
   const galleryItemElement = galleryItemTemplate.cloneNode(true);
   galleryItemElement.querySelector('.photo-gallery__name').textContent = name;
   galleryItemElement.querySelector('.photo-gallery__like-btn').addEventListener('click', evt => {
@@ -83,16 +89,18 @@ function addCard(name, link) {
 
   const imageElement = galleryItemElement.querySelector('.photo-gallery__image');
   imageElement.src = link;
+  imageElement.alt = alt;
   imageElement.addEventListener('click', () => {
     togglePopUp(popUpViewImage);
     srcPopUpViewImage.src = link;
+    srcPopUpViewImage.alt = alt;
     paragraphPopUpViewImage.textContent = name;
   });
 
   return galleryItemElement;
 }
 
-function renderCard(name, link) {
-  gallery.prepend(addCard(name, link));
+function renderCard(name, link, alt) {
+  gallery.prepend(addCard(name, link, alt));
 }
 
